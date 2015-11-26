eebd35200ee9f9b92f7989c8195f5f96dac3a7ed
yuliang136 <109827298@qq.com>
2015-11-26 16:37:27 +0800

    上传了项目
    上传了项目

---
+++ BigwordOfFirstHead/BigwordOfFirstHead/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigwordOfFirstHead
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Shout());
+        }
+
+        public static string Shout()
+        {
+            return "喵";
+        }
+    }
+}

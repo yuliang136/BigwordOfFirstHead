2299a9fe498c32dc141c9cf6f8e62220a03737b8
yuliang136 <109827298@qq.com>
2015-11-27 16:15:00 +0800

    1.3
    1.3

--- SimpleFactoryMode/SimpleFactoryMode/Program.cs
+++ SimpleFactoryMode/SimpleFactoryMode/Program.cs
@@ -10,29 +10,29 @@ namespace SimpleFactoryMode
     {
         static void Main(string[] args)
         {
-            string FirstNum = Console.ReadLine();
-            string Operation = Console.ReadLine();
-            string SecondNum = Console.ReadLine();
-            string Result = "";
+            string _strFirstNum = Console.ReadLine();
+            string _strOperation = Console.ReadLine();
+            string _strSecondNum = Console.ReadLine();
+            string _strResult = "";
 
-            if (Operation == "+")
+            switch (_strOperation)
             {
-                Result = Convert.ToString(Convert.ToDouble(FirstNum) + Convert.ToDouble(SecondNum));
-            }
-            else if(Operation == "-")
-            {
-                Result = Convert.ToString(Convert.ToDouble(FirstNum) - Convert.ToDouble(SecondNum));
-            }
-            else if (Operation == "*")
-            {
-                Result = Convert.ToString(Convert.ToDouble(FirstNum) * Convert.ToDouble(SecondNum));
-            }
-            else if (Operation == "/")
-            {
-                Result = Convert.ToString(Convert.ToDouble(FirstNum) / Convert.ToDouble(SecondNum));
+                case "+":
+                    _strResult = Convert.ToString(Convert.ToDouble(_strFirstNum) + Convert.ToDouble(_strSecondNum));
+                    break;
+                case "-":
+                    _strResult = Convert.ToString(Convert.ToDouble(_strFirstNum) - Convert.ToDouble(_strSecondNum));
+                    break;
+                case "*":
+                    _strResult = Convert.ToString(Convert.ToDouble(_strFirstNum) * Convert.ToDouble(_strSecondNum));
+                    break;
+                case "/":
+                    _strResult = Convert.ToString(Convert.ToDouble(_strFirstNum) / Convert.ToDouble(_strSecondNum));
+                    break;
+
             }
 
-            Console.WriteLine("Reuslt is {0}", Result);
+            Console.WriteLine("Reuslt is {0}", _strResult);
         }
     }
 }

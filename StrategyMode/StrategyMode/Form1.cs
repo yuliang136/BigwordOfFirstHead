3ead42a2ef9e8cda76b171e7c57420baee518575
yuliang136 <109827298@qq.com>
2015-11-27 17:43:35 +0800

    2.1
    2.1

---
+++ StrategyMode/StrategyMode/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StrategyMode
+{
+    public partial class Form1 : Form
+    {
+        private double total = 0.0d;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double totalPrices = Convert.ToDouble(txtPrice.Text)*Convert.ToDouble(txtNum.Text);
+
+            total += totalPrices;
+
+            lbxList.Items.Add("单价： " + txtPrice.Text + " 数量: "
+                              + txtNum.Text + " 合计： "
+                              + totalPrices.ToString());
+
+            lblResult.Text = total.ToString();
+        }
+    }
+}

da87c4814042b4ab9f0f03757530904026586687
yuliang136 <109827298@qq.com>
2015-11-27 16:11:34 +0800

    1.1
    1.1

---
+++ SimpleFactoryMode/SimpleFactoryMode/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactoryMode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FirstNum = Console.ReadLine();
+            string Operation = Console.ReadLine();
+            string SecondNum = Console.ReadLine();
+            string Result = "";
+
+            if (Operation == "+")
+            {
+                Result = Convert.ToString(Convert.ToDouble(FirstNum) + Convert.ToDouble(SecondNum));
+            }
+            else if(Operation == "-")
+            {
+                Result = Convert.ToString(Convert.ToDouble(FirstNum) - Convert.ToDouble(SecondNum));
+            }
+            else if (Operation == "*")
+            {
+                Result = Convert.ToString(Convert.ToDouble(FirstNum) * Convert.ToDouble(SecondNum));
+            }
+            else if (Operation == "/")
+            {
+                Result = Convert.ToString(Convert.ToDouble(FirstNum) / Convert.ToDouble(SecondNum));
+            }
+
+            Console.WriteLine("Reuslt is {0}", Result);
+        }
+    }
+}

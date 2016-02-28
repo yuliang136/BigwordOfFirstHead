d85c8cec11443f1fc4bc4b7846f3e109d6471ead
moonlight <moonlightwork2016@outlook.com>
2016-02-28 11:53:13 +0800

    StateMode Commit
    StateMode Commit

---
+++ StateMode/StateMode/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateMode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nHour = 10;
+
+            StateMode stateMode = new StateMode();
+            stateMode.RunProgram(nHour);
+        }
+    }
+}
